ae4e658067ab7c6726718ff3ab174551e1b18970
Alex <beccialex2002@gmail.com>
2023-02-04 15:18:16 -0300

    Router change

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <div className="bg-zinc-800 h-screen text-white">
-      <div className="flex items-center justify-center h-full ">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<TaskList/>} />
-          <Route path="/create-task" element={<TaskForm />} />
-          <Route path="/edit-task/:id" element={<TaskForm/>} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div className="bg-zinc-800 h-screen text-white">
+        <div className="flex items-center justify-center h-full ">
+          <Routes>
+            <Route path="/" element={<TaskList />} />
+            <Route path="/create-task" element={<TaskForm />} />
+            <Route path="/edit-task/:id" element={<TaskForm />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
