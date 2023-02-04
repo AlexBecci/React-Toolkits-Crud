a45b7654529921bbae16846b82353adae488f25d
Alex <beccialex2002@gmail.com>
2023-02-04 15:17:10 -0300

    Task 3 Example added

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -12,6 +12,12 @@ const initialState = [
     description: "Task 2 description",
     completed: false,
   },
+  {
+    id: "3",
+    title: "Task 3",
+    description: "Task 3 description",
+    completed: false,
+  },
 ];
 
 export const taskSlice = createSlice({
